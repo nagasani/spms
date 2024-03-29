ba169f679db6623af67e0ed2c68bb0276403e055
nagasani <nagasani.r@gmail.com>
2024-03-29 12:54:15 -0400

    Kafka

--- src/main/java/com/spms/sports/service/KafkaBookConsumerService.java
+++ src/main/java/com/spms/sports/service/KafkaBookConsumerService.java
@@ -4,22 +4,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.spms.sports.entity.Book;
 
 @Service
 public class KafkaBookConsumerService 
 {
     private static final Logger logger = LoggerFactory.getLogger(KafkaBookConsumerService.class);
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    
+    private final ObjectMapper objectMapper;
 
+    public KafkaBookConsumerService() 
+    {
+        this.objectMapper = new ObjectMapper();
+        // Enable pretty printing
+        this.objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
+    
     @KafkaListener(topics = "new-books", groupId = "book-consumers")
-    public void listenForBookMessages(String message) {
+    public void listenForBookMessages(String message) 
+    {
         try 
         {
             Book book = objectMapper.readValue(message, Book.class);
-            logger.info("Received message: {}", book);            
+            // Convert the book object back to a pretty JSON string for logging
+            String prettyBookJson = objectMapper.writeValueAsString(book);
+            logger.info("Received message: {}", prettyBookJson);            
         } 
         catch (Exception e) 
         {
